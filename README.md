0ee204b31efbe0d2eea9739201d4f1305cebc5fd
Rene <68439248+Rene-goon@users.noreply.github.com>
2024-07-16 16:08:03 -0500

    setting up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GmailCleanUp
 uses gmail's api to clear up some space in gmail automatically 
+
+# Setup Instructions
+
+1. Copy `config.example.ini` contents to `config.ini`.
+2. Fill in the necessary values in `config.ini` with your own information.
+3. Save the file.
---
+++ clean-up.py
@@ -0,0 +1,9 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+# optional to catch errs
+from googleapiclient.errors import HttpError
+
+CLIENT_FILE = ''

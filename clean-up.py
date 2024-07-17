8a9f600fe30499247f2ab885f8053d98d19f318a
Rene <68439248+Rene-goon@users.noreply.github.com>
2024-07-17 14:46:58 -0500

    connect to google api

--- clean-up.py
+++ clean-up.py
@@ -1,4 +1,4 @@
-import os
+import os, configparser
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -6,4 +6,32 @@ from googleapiclient.discovery import build
 # optional to catch errs
 from googleapiclient.errors import HttpError
 
-CLIENT_FILE = ''
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+CLIENT_FILE = config['paths']['client_field']
+SCOPES = ['https://mail.google.com/']
+
+creds = None
+
+if os.path.exists('token.json'):
+    credits = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+if not creds or not creds.valid: 
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else: 
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_FILE, SCOPES)
+        creds = flow.run_local_server(port=0)
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json()) 
+
+# connect to api service
+service_gmail = build('gmail', 'v1', credentials=creds)
+print(dir(service_gmail))
+print("Successfully authenticated and obtained credentials.")
+
+
+# with open(CLIENT_FILE, 'r') as file:
+#     content = file.read()
+#     print('HERE: ' + content)
